00ea474b3d2c1239fafb16396d7b4f2c712d3fc2
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-06-26 18:32:14 -0400

    Expose the get all and get one functionality as new views

--- libre/apps/data_drivers/urls.py
+++ libre/apps/data_drivers/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('data_drivers.views',
-    url(r'^(?P<endpoint>\w+)/(?P<resource>\w+)/(?P<id>\w+)/$', 'resource_get', (), 'resource_get'),
+    url(r'^(?P<endpoint>\w+)/(?P<resource>\w+)/all/$', 'resource_get_all', (), 'resource_get_all'),
+    url(r'^(?P<endpoint>\w+)/(?P<resource>\w+)/(?P<id>\w+)/$', 'resource_get_one', (), 'resource_get_one'),
 )
--- libre/apps/data_drivers/views.py
+++ libre/apps/data_drivers/views.py
@@ -1,21 +1,27 @@
 from __future__ import absolute_import
 
+import json
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.core.urlresolvers import reverse
-from django.utils.http import urlencode
 
 from .classes import SampleExcel
 
+source = SampleExcel()
 
-def resource_get(request, endpoint, resource, id):
-    source = SampleExcel()
+
+def resource_get_all(request, endpoint, resource):
+    data = source.all()
+
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+def resource_get_one(request, endpoint, resource, id):
     data = source.get(int(id))
 
-    return render_to_response('data.html', {
-        'data': data,
-    }, context_instance=RequestContext(request))
+    return HttpResponse(json.dumps(data), content_type="application/json")
