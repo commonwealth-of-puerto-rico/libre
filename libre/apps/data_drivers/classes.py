a4be5530f5b2d3c0cd118cddc0487247c33cdad7
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-06-26 18:27:47 -0400

    Add default values, add support for getting all of the resources in a single call, add support for zero base ids

--- libre/apps/data_drivers/classes.py
+++ libre/apps/data_drivers/classes.py
@@ -1,9 +1,13 @@
+from __future__ import absolute_import
+
 import os
 import string
 
 from django.conf import settings
 
 import xlrd
+
+from .literals import DEFAULT_FIRST_ROW_NAMES, DEFAULT_LIMIT, DEFAULT_ZERO_BASE
 
 
 class Resource(object):
@@ -15,7 +19,8 @@ class Resource(object):
 
 
 class Source(object):
-    # limit
+    limit = DEFAULT_LIMIT
+    zero_base = DEFAULT_ZERO_BASE
 
     def __init__(self):
         pass
@@ -27,6 +32,8 @@ class Excel(Source):
     # column_names: List
     # first_row_names: Boolean
 
+    first_row_names = DEFAULT_FIRST_ROW_NAMES
+
     def __init__(self):
         self._book = xlrd.open_workbook(self.file_path)
         try:
@@ -34,12 +41,19 @@ class Excel(Source):
         except TypeError:
             self._sheet = self._book.sheet_by_name(self.sheet)
 
-        if getattr(self, 'first_row_names', False):
+        if self.first_row_names:
             self.column_names = [cell.value for cell in self._sheet.row(0)]
 
     def get(self, id):
-        if getattr(self, 'first_row_names', False):
+        if not self.zero_base and id == 0:
+            raise Exception  # TODO: And id of 0 was specified and this resource don't allow them
+
+        if self.first_row_names:
             id = id + 1
+
+        if not self.zero_base:
+            id = id - 1
+
         result = {}
         column_count = 0
         column_names = getattr(self, 'column_names', string.ascii_uppercase)
@@ -50,9 +64,20 @@ class Excel(Source):
 
         return Resource(result)
 
+    def all(self):
+        result = []
+        for id in xrange(0 if self.zero_base else 1, self.limit):
+            try:
+                result.append(self.get(id))
+            except IndexError:
+                break
+
+        return result
+
 
 class SampleExcel(Excel):
     file_path = os.path.join(settings.PROJECT_ROOT, 'contrib', 'sample.xls')
+    #file_path = os.path.join(settings.PROJECT_ROOT, '..', 'CorpsBAK.xlsx')
     sheet = 0
     column_names = ['First name', 'Last name', 'Region', 'Office']
     first_row_names = True
