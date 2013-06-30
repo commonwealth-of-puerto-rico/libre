c60638eb09c3d586c3a4632a8946e9b5e606b46b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-06-30 03:01:23 -0400

    Move file watching code out of __init__ and into it's own file

--- libre/apps/data_drivers/__init__.py
+++ libre/apps/data_drivers/__init__.py
@@ -1,19 +1,5 @@
 from __future__ import absolute_import
 
-import pyinotify
+from .file_watcher import setup_file_watcher
 
-from .models import SourceSpreadsheet
-
-
-class MyEventHandler(pyinotify.ProcessEvent):
-    def process_IN_MODIFY(self, event):
-        print "MODIFY event:", event.pathname
-        for source in SourceSpreadsheet.objects.filter(path=event.pathname):
-            source.import_data()
-
-
-wm = pyinotify.WatchManager()
-notifier = pyinotify.ThreadedNotifier(wm, default_proc_fun=MyEventHandler())
-for source in SourceSpreadsheet.objects.exclude(path__exact=''):
-    wm.add_watch(source.path, pyinotify.IN_MODIFY, rec=True)#, auto_add=True)
-notifier.start()
+setup_file_watcher()
