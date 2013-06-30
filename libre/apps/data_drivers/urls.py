293f4228e771ab13e23b72cc5314dbd43abc8bf3
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-06-30 03:55:40 -0400

    Make resturning all resources implicit in the URL

--- libre/apps/data_drivers/urls.py
+++ libre/apps/data_drivers/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('data_drivers.views',
     url(r'^resource/all/$', 'resource_list', (), 'resource_list'),
-    url(r'^resource/(?P<resource_slug>\w+)/all/$', 'resource_get_all', (), 'resource_get_all'),
+    url(r'^resource/(?P<resource_slug>\w+)/$', 'resource_get_all', (), 'resource_get_all'),
     url(r'^resource/(?P<resource_slug>\w+)/(?P<id>\w+)/$', 'resource_get_one', (), 'resource_get_one'),
 )
