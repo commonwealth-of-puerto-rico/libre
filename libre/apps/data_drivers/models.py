a7af3d00093a4f81ddc2c6c7f22a27eb4d278439
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-07-02 15:07:17 -0400

    Merge some fields and methods using an abstract class

--- libre/apps/data_drivers/models.py
+++ libre/apps/data_drivers/models.py
@@ -42,10 +42,104 @@ class Source(models.Model):
         verbose_name_plural = _('sources')
 
 
-class SourceSpreadsheet(Source):
+class SourceFileBased(Source):
     limit = models.PositiveIntegerField(default=DEFAULT_LIMIT, verbose_name=_('limit'), help_text=('Maximum number of items to show when all items are requested.'))
     path = models.TextField(blank=True, null=True, verbose_name=_('path to file'), help_text=('Location to a file in the filesystem.'))
-    file = models.FileField(blank=True, null=True, upload_to='spreadsheets', verbose_name=_('upload a file'))
+    file = models.FileField(blank=True, null=True, upload_to='spreadsheets', verbose_name=_('uploaded file'))
+
+    def get_one(self, id, timestamp=None):
+        if timestamp:
+            source_data_version = self.sourcedataversion_set.get(timestamp=timestamp)
+        else:
+            source_data_version = self.sourcedataversion_set.get(active=True)
+
+        return SourceData.objects.get(source_data_version=source_data_version, row_id=id).row
+
+    def get_all(self, timestamp=None):
+        try:
+            if timestamp:
+                source_data_version = self.sourcedataversion_set.get(timestamp=timestamp)
+            else:
+                source_data_version = self.sourcedataversion_set.get(active=True)
+        except SourceDataVersion.DoesNotExist:
+            return []
+
+        return [item.row for item in SourceData.objects.filter(source_data_version=source_data_version)[0:self.limit]]
+
+    class Meta:
+        abstract = True
+
+
+class SourceCSV(SourceFileBased):
+    column_names = models.TextField(blank=True, verbose_name=_('column names'), help_text=('Specify the column names to use.'))
+    first_row_names = models.BooleanField(default=DEFAULT_FIRST_ROW_NAMES, verbose_name=_('first row names'), help_text=('Use the values of the first row as the column names.'))
+    delimiter = models.CharField(blank=True, max_length=1, default=',', verbose_name=_('delimiter'))
+    quote_character = models.CharField(blank=True, max_length=1, verbose_name=_('quote character'))
+    column_widths = models.TextField(blank=True, null=True, verbose_name=_('column widths'), help_text=_('The column widths separated by a comma.'))
+
+    def _get_items(self):
+        column_names = self.column_names or string.ascii_uppercase
+
+        if self.column_widths:
+            fmtstring = ''.join('%ds' % f for f in map(int, self.column_widths.split(',')))
+            parse = struct.Struct(fmtstring).unpack_from
+
+            if self.first_row_names:
+                column_names = map(string.strip, parse(self._file_handle.next()))
+
+            for row in self._file_handle:
+                yield dict(zip(column_names, map(string.strip, parse(row))))
+        else:
+            kwargs = {}
+            if self.delimiter:
+                kwargs['delimiter'] = str(self.delimiter)
+            if self.quote_character:
+                kwargs['quotechar'] = str(self.quote_character)
+
+            reader = csv.reader(self._file_handle, **kwargs)
+
+            if self.first_row_names:
+                column_names = reader.next()
+
+            for row in reader:
+                yield dict(zip(column_names, row))
+
+    def import_data(self):
+        if self.path:
+            new_hash = HASH_FUNCTION(open(self.path).read())
+        else:
+            new_hash = HASH_FUNCTION(self.path.open().read())
+
+        try:
+            source_data_version = self.sourcedataversion_set.get(checksum=new_hash)
+        except SourceDataVersion.DoesNotExist:
+            source_data_version = SourceDataVersion.objects.create(source=self, checksum=new_hash)
+
+            if self.path:
+                self._file_handle = open(self.path)
+            else:
+                self._file_handle = self.file.open()
+
+            id = 1
+            for row in self._get_items():
+                SourceData.objects.create(source_data_version=source_data_version, row_id=id, row=row)
+                id = id + 1
+
+            self._file_handle.close()
+
+            source_data_version.ready = True
+            source_data_version.active = True
+            source_data_version.save()
+        else:
+            source_data_version.active = True
+            source_data_version.save()
+
+    class Meta:
+        verbose_name = _('CSV source')
+        verbose_name_plural = _('CSV sources')
+
+
+class SourceSpreadsheet(SourceFileBased):
     sheet = models.CharField(max_length=32, default=DEFAULT_SHEET, verbose_name=_('sheet'), help_text=('Worksheet of the spreadsheet file to use.'))
     column_names = models.TextField(blank=True, verbose_name=_('column names'), help_text=('Specify the column names to use.'))
     first_row_names = models.BooleanField(default=DEFAULT_FIRST_ROW_NAMES, verbose_name=_('first row names'), help_text=('Use the values of the first row as the column names.'))
@@ -130,22 +224,6 @@ class SourceSpreadsheet(Source):
             source_data_version.active = True
             source_data_version.save()
 
-    def get_one(self, id, timestamp=None):
-        if timestamp:
-            source_data_version = self.sourcedataversion_set.get(timestamp=timestamp)
-        else:
-            source_data_version = self.sourcedataversion_set.get(active=True)
-
-        return SourceData.objects.get(source_data_version=source_data_version, row_id=id).row
-
-    def get_all(self, timestamp=None):
-        if timestamp:
-            source_data_version = self.sourcedataversion_set.get(timestamp=timestamp)
-        else:
-            source_data_version = self.sourcedataversion_set.get(active=True)
-
-        return [item.row for item in SourceData.objects.filter(source_data_version=source_data_version)[0:self.limit]]
-
     class Meta:
         verbose_name = _('spreadsheet source')
         verbose_name_plural = _('spreadsheet sources')
@@ -183,93 +261,3 @@ class SourceData(models.Model):
         verbose_name = _('source data')
         verbose_name_plural = _('sources data')
 
-
-class SourceCSV(Source):
-    limit = models.PositiveIntegerField(default=DEFAULT_LIMIT, verbose_name=_('limit'), help_text=('Maximum number of items to show when all items are requested.'))
-    path = models.TextField(blank=True, null=True, verbose_name=_('path to file'), help_text=('Location to a file in the filesystem.'))
-    file = models.FileField(blank=True, null=True, upload_to='spreadsheets', verbose_name=_('uploaded file'))
-    column_names = models.TextField(blank=True, verbose_name=_('column names'), help_text=('Specify the column names to use.'))
-    first_row_names = models.BooleanField(default=DEFAULT_FIRST_ROW_NAMES, verbose_name=_('first row names'), help_text=('Use the values of the first row as the column names.'))
-    delimiter = models.CharField(blank=True, max_length=1, default=',', verbose_name=_('delimiter'))
-    quote_character = models.CharField(blank=True, max_length=1, verbose_name=_('quote character'))
-    column_widths = models.TextField(blank=True, null=True, verbose_name=_('column widths'), help_text=_('The column widths separated by a comma.'))
-
-    def _get_items(self):
-        column_names = self.column_names or string.ascii_uppercase
-
-        if self.column_widths:
-            fmtstring = ''.join('%ds' % f for f in map(int, self.column_widths.split(',')))
-            parse = struct.Struct(fmtstring).unpack_from
-
-            if self.first_row_names:
-                column_names = map(string.strip, parse(self._file_handle.next()))
-
-            for row in self._file_handle:
-                yield dict(zip(column_names, map(string.strip, parse(row))))
-        else:
-            kwargs = {}
-            if self.delimiter:
-                kwargs['delimiter'] = str(self.delimiter)
-            if self.quote_character:
-                kwargs['quotechar'] = str(self.quote_character)
-
-            reader = csv.reader(self._file_handle, **kwargs)
-
-            if self.first_row_names:
-                column_names = reader.next()
-
-            for row in reader:
-                yield dict(zip(column_names, row))
-
-    def import_data(self):
-        if self.path:
-            new_hash = HASH_FUNCTION(open(self.path).read())
-        else:
-            new_hash = HASH_FUNCTION(self.path.open().read())
-
-        try:
-            source_data_version = self.sourcedataversion_set.get(checksum=new_hash)
-        except SourceDataVersion.DoesNotExist:
-            source_data_version = SourceDataVersion.objects.create(source=self, checksum=new_hash)
-
-            if self.path:
-                self._file_handle = open(self.path)
-            else:
-                self._file_handle = self.file.open()
-
-            id = 1
-            for row in self._get_items():
-                SourceData.objects.create(source_data_version=source_data_version, row_id=id, row=row)
-                id = id + 1
-
-            self._file_handle.close()
-
-            source_data_version.ready = True
-            source_data_version.active = True
-            source_data_version.save()
-        else:
-            source_data_version.active = True
-            source_data_version.save()
-
-    def get_one(self, id, timestamp=None):
-        if timestamp:
-            source_data_version = self.sourcedataversion_set.get(timestamp=timestamp)
-        else:
-            source_data_version = self.sourcedataversion_set.get(active=True)
-
-        return SourceData.objects.get(source_data_version=source_data_version, row_id=id).row
-
-    def get_all(self, timestamp=None):
-        try:
-            if timestamp:
-                source_data_version = self.sourcedataversion_set.get(timestamp=timestamp)
-            else:
-                source_data_version = self.sourcedataversion_set.get(active=True)
-        except SourceDataVersion.DoesNotExist:
-            return []
-
-        return [item.row for item in SourceData.objects.filter(source_data_version=source_data_version)[0:self.limit]]
-
-    class Meta:
-        verbose_name = _('CSV source')
-        verbose_name_plural = _('CSV sources')
