2d82dfaaead7d86697d477e8c8eb54e9491c064a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-07-09 02:53:33 -0400

    Add support for passing the current context data to the leaflet template, add initial GeoJSON rendering

--- libre/apps/data_drivers/renderers.py
+++ libre/apps/data_drivers/renderers.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import logging
+import json
 
 from rest_framework import renderers
 
@@ -8,3 +9,28 @@ from rest_framework import renderers
 class LeafletRenderer(renderers.TemplateHTMLRenderer):
     template_name = 'leaflet.html'
     format = 'leaflet'
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        """
+        Renders data to HTML, using Django's standard template rendering.
+
+        The template name is determined by (in order of preference):
+
+        1. An explicit .template_name set on the response.
+        2. An explicit .template_name set on this class.
+        3. The return result of calling view.get_template_names().
+        """
+        renderer_context = renderer_context or {}
+        view = renderer_context['view']
+        request = renderer_context['request']
+        response = renderer_context['response']
+
+        if response.exception:
+            template = self.get_exception_template(response)
+        else:
+            template_names = self.get_template_names(response, view)
+            template = self.resolve_template(template_names)
+
+        context = self.resolve_context(data, request, response)
+        context.update({'data': json.dumps(data)})
+        return template.render(context)
