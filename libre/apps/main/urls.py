f232738a4bfb3a85d1915166da989fcd297011c8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-08-16 04:02:37 -0400

    Add favicon

--- libre/apps/main/urls.py
+++ libre/apps/main/urls.py
@@ -1,5 +1,11 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, url
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('main.views',
     url(r'^$', 'home', (), 'home'),
+)
+
+urlpatterns += patterns('',
+    url(r'^favicon\.ico$', RedirectView.as_view(url='%simages/favicon.ico' % settings.STATIC_URL), name='favicon'),
 )
