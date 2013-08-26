425574bc0eb335b85d5c45718e22dc714b46dcc5
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-08-26 03:28:27 -0400

    Add basic HTTP authentication

--- libre/settings.py
+++ libre/settings.py
@@ -219,6 +219,7 @@ except ImportError:
 # REST FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
