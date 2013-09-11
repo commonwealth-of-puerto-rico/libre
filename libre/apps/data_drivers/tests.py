b0b251401417a54c41a771f843d0eb6e91f4ee07
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-09-11 11:44:35 -0400

    Improve data_drivers app test units

--- libre/apps/data_drivers/tests.py
+++ libre/apps/data_drivers/tests.py
@@ -11,11 +11,16 @@ from .models import SourceCSV, SourceFixedWidth
 class SourceTestCase(TestCase):
     def setUp(self):
         origin_url = OriginURLFile.objects.create(label='test origin', url='http://www.census.gov/population/estimates/puerto-rico/prmunnet.txt')
-        SourceFixedWidth.objects.create(name='test fixed width source', origin=origin_url)
+        SourceFixedWidth.objects.create(name='test fixed width source', origin=origin_url, limit=200)
 
-    def test_source_slug_creation(self):
+    def test_fw_source_clear_versions(self):
         fw_source = SourceFixedWidth.objects.get(name='test fixed width source')
-        self.assertEqual(fw_source.slug, 'test-fixed-width-source')
+        fw_source.check_source_data()
+        self.assertEqual(fw_source.versions.count(), 1)
+        version = fw_source.versions.all()[0]
+        self.assertEqual(version.data.count(), 95)
+        fw_source.clear_versions()
+        self.assertEqual(fw_source.versions.count(), 0)
 
     def test_fw_source_simple_import(self):
         fw_source = SourceFixedWidth.objects.get(name='test fixed width source')
@@ -26,7 +31,6 @@ class SourceTestCase(TestCase):
 
     def test_fw_source_column_skip_regex(self):
         fw_source = SourceFixedWidth.objects.get(name='test fixed width source')
-        fw_source.clear_versions()
         fw_source.columns.create(name='city', size=18, data_type=DATA_TYPE_STRING, skip_regex=r'(?:Municipio.*|\||-.*|Puerto Rico)')
         fw_source.columns.create(name='1999_estimate', size=15, data_type=DATA_TYPE_NUMBER)
         fw_source.columns.create(name='1990_census', size=16, data_type=DATA_TYPE_NUMBER)
@@ -40,7 +44,22 @@ class SourceTestCase(TestCase):
         version = fw_source.versions.all()[0]
         self.assertEqual(version.data.count(), 78)
 
-    def test_fw_source_queries_aggregate_count(self):
+
+class QueryTestCase(TestCase):
+    def setUp(self):
+        origin_url = OriginURLFile.objects.create(label='test origin', url='http://www.census.gov/population/estimates/puerto-rico/prmunnet.txt')
+        SourceFixedWidth.objects.create(name='test fixed width source', origin=origin_url, limit=200)
+
+    def test_aggregate_count(self):
         fw_source = SourceFixedWidth.objects.get(name='test fixed width source')
+        fw_source.columns.create(name='city', size=18, data_type=DATA_TYPE_STRING, skip_regex=r'(?:Municipio.*|\||-.*|Puerto Rico)')
+        fw_source.columns.create(name='1999_estimate', size=15, data_type=DATA_TYPE_NUMBER)
+        fw_source.columns.create(name='1990_census', size=16, data_type=DATA_TYPE_NUMBER)
+        fw_source.columns.create(name='percent_change', size=17, data_type=DATA_TYPE_NUMBER)
+        fw_source.columns.create(name='births', size=10, data_type=DATA_TYPE_NUMBER)
+        fw_source.columns.create(name='deaths', size=10, data_type=DATA_TYPE_NUMBER)
+        fw_source.columns.create(name='natural_increase', size=18, data_type=DATA_TYPE_NUMBER)
+        fw_source.columns.create(name='net_migration', size=15, data_type=DATA_TYPE_NUMBER)
+        fw_source.check_source_data()
         result = list(fw_source.get_all(parameters={'_aggregate__total' :'Count(*)'}))
         self.assertEqual(result, [{'total': 78}])
