42dcf8cef21f7fd805ac4b33802201cfcae16b07
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-11-07 16:53:46 -0400

    Disabled the Django admin documentation generator, its setup is incomplete and not adding any additional value, should close issue #8.

--- libre/urls.py
+++ libre/urls.py
@@ -10,7 +10,6 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^', include('main.urls')),
