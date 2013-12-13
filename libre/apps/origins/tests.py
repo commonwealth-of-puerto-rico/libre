dc51492e837cac91592721a3685b4336d31ae394
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-12-12 22:49:29 -0400

    Update tests

--- libre/apps/origins/tests.py
+++ libre/apps/origins/tests.py
@@ -21,8 +21,6 @@ def temp_path_status(path):
 
 
 class URLFileTestCase(TestCase):
-
-
     def setUp(self):
         self.origin_url = OriginURLFile.objects.create(label='test origin', url='http://www.census.gov/population/estimates/puerto-rico/prmunnet.txt')
         self.origin_url.copy_data()
@@ -30,23 +28,20 @@ class URLFileTestCase(TestCase):
     def tearDown(self):
         self.origin_url.discard_copy()
 
-    def test_temp_file_creation(self):
-        self.assertEqual(temp_path_status(self.origin_url.temporary_file.name), 'File exists')
-
     def test_copy_file_creation(self):
         self.assertEqual(temp_path_status(self.origin_url.copy_file.name), 'File exists')
 
     def test_hash(self):
-        self.assertEqual(self.origin_url.new_hash, 'df0cfc653167d570a6aecfbab0f33e89377a0430dd74d92338a1802567a12621')
+        self.assertEqual(self.origin_url.new_hash, '81f81877c664b9863628e253ebfdff2cc53b05cbf8020735cb37eef46901ebe8')
 
     def test_iterator(self):
-        self.assertEqual(self.origin_url.data_iterator.next(), 'PR-99-1 Estimates of the Population of Puerto Rico Municipios, July 1, 1999, and')
-        self.assertEqual(self.origin_url.data_iterator.next(), 'Demographic Components of Population Change: April 1, 1990 to July 1, 1999')
-        self.origin_url.temporary_file.seek(0)
-        self.assertEqual(self.origin_url.data_iterator.next(), 'PR-99-1 Estimates of the Population of Puerto Rico Municipios, July 1, 1999, and')
+        self.assertEqual(self.origin_url.copy_file.next(), 'PR-99-1 Estimates of the Population of Puerto Rico Municipios, July 1, 1999, and\r\n')
+        self.assertEqual(self.origin_url.copy_file.next(), 'Demographic Components of Population Change: April 1, 1990 to July 1, 1999\r\n')
+        self.origin_url.copy_file.seek(0)
+        self.assertEqual(self.origin_url.copy_file.next(), 'PR-99-1 Estimates of the Population of Puerto Rico Municipios, July 1, 1999, and\r\n')
 
     def test_copy_file(self):
-        self.assertEqual(len(self.origin_url.copy_file.read()), 10618)
+        self.assertEqual(len(self.origin_url.copy_file.read()), 10713)
         self.assertEqual(len(self.origin_url.copy_file.read()), 0)
 
 
@@ -58,21 +53,18 @@ class OriginPathTestCase(TestCase):
     def tearDown(self):
         self.origin_url.discard_copy()
 
-    def test_temp_file_creation(self):
-        self.assertEqual(temp_path_status(self.origin_url.temporary_file.name), 'File exists')
-
     def test_copy_file_creation(self):
         self.assertEqual(temp_path_status(self.origin_url.copy_file.name), 'File exists')
 
     def test_hash(self):
-        self.assertEqual(self.origin_url.new_hash, 'a860d5522702a5aeb002a6b7aa21d24abb999cd65723504414cd5f8b5bb8d931')
+        self.assertEqual(self.origin_url.new_hash, '81f81877c664b9863628e253ebfdff2cc53b05cbf8020735cb37eef46901ebe8')
 
     def test_iterator(self):
-        self.assertEqual(self.origin_url.data_iterator.next(), 'PR-99-1 Estimates of the Population of Puerto Rico Municipios, July 1, 1999, and\r\n')
-        self.assertEqual(self.origin_url.data_iterator.next(), 'Demographic Components of Population Change: April 1, 1990 to July 1, 1999\r\n')
-        self.origin_url.temporary_file.seek(0)
-        self.assertEqual(self.origin_url.data_iterator.next(), 'PR-99-1 Estimates of the Population of Puerto Rico Municipios, July 1, 1999, and\r\n')
+        self.assertEqual(self.origin_url.copy_file.next(), 'PR-99-1 Estimates of the Population of Puerto Rico Municipios, July 1, 1999, and\r\n')
+        self.assertEqual(self.origin_url.copy_file.next(), 'Demographic Components of Population Change: April 1, 1990 to July 1, 1999\r\n')
+        self.origin_url.copy_file.seek(0)
+        self.assertEqual(self.origin_url.copy_file.next(), 'PR-99-1 Estimates of the Population of Puerto Rico Municipios, July 1, 1999, and\r\n')
 
     def test_copy_file(self):
-        self.assertEqual(len(self.origin_url.copy_file.read()), 10808)
+        self.assertEqual(len(self.origin_url.copy_file.read()), 10713)
         self.assertEqual(len(self.origin_url.copy_file.read()), 0)
